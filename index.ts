4f01d63318f57a756b99431feb106c86385e325d
contextCue <14878694+ed42311@users.noreply.github.com>
2020-10-07 16:27:40 -0600

    Person query working

--- index.ts
+++ index.ts
@@ -1,43 +1,48 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as gcp from "@pulumi/gcp";
-import { ApolloServer, gql } from "apollo-server-cloud-functions";
+import { config } from 'dotenv'
+import * as gcp from '@pulumi/gcp'
+import { ApolloServer } from 'apollo-server-cloud-functions'
+import neo4j from 'neo4j-driver'
+
+// eslint-disable-next-line
+const neo4jGraphqlJs = require('neo4j-graphql-js')
+const { makeAugmentedSchema } = neo4jGraphqlJs
+
+config()
 
 function factory() {
-    // Construct a schema, using GraphQL schema language
-    const typeDefs = gql`
-        type Query {
-            hello: String
-        }
-    `;
+  const typeDefs = `
+type Person {
+    name: String
+}
+`
+  const schema = makeAugmentedSchema({ typeDefs })
+  const driver = neo4j.driver(
+    'bolt://18.208.119.122:32864',
+    neo4j.auth.basic('neo4j', 'floor-custody-directions')
+  )
 
-    // Provide resolver functions for your schema fields
-    const resolvers = {
-        Query: {
-            hello: () => 'Hello world!',
-        },
-    };
+  const server = new ApolloServer({
+    schema,
+    introspection: true,
+    playground: {
+      endpoint: '/dev/graphql',
+    },
+    context: { driver },
+  })
 
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        introspection: true,
-        playground: {
-          endpoint: '/dev/graphql',
-        },
-    });
-
-    const cb = server.createHandler();
-    return cb;
+  const cb = server.createHandler()
+  return cb
 }
 
-const apiFunction = new gcp.cloudfunctions.HttpCallbackFunction("apiFunction", {
-    callbackFactory: factory,
-});
+const apiFunction = new gcp.cloudfunctions.HttpCallbackFunction('apiFunction', {
+  callbackFactory: factory,
+})
 
-const apiInvoker = new gcp.cloudfunctions.FunctionIamMember("apiInvoker", {
-    cloudFunction: apiFunction.function.id,
-    member: "allUsers",
-    role: "roles/cloudfunctions.invoker",
-});
+// eslint-disable-next-line
+const apiInvoker = new gcp.cloudfunctions.FunctionIamMember('apiInvoker', {
+  cloudFunction: apiFunction.function.id,
+  member: 'allUsers',
+  role: 'roles/cloudfunctions.invoker',
+})
 
-export const url = apiFunction.httpsTriggerUrl;
+export const url = apiFunction.httpsTriggerUrl
